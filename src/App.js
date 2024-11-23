fdf1084c712d305d5ce8559345cfa47a1f2c3ca8
mohamed <mohamedalsaifi00@gmail.com>
2024-11-23 23:51:19 +0200

    calic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
 
-function App() {
+const App = () => {
+  const [result, setresult] = useState("");
+  const ClearCalic = () => {
+    setresult("");
+  };
+  const getValue = (e) => {
+    setresult(result.concat(e.target.name));
+  };
+  const deleteCalic = () => {
+    setresult(result.slice(0, result.length - 1));
+  };
+  const CalicValue = () => {
+    try {
+      setresult(eval(result).toString());
+    } catch (error) {
+      setresult("The value is incorrect.");
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <h1 className="title"> Calculator</h1>
+
+      <div className="container_Calic">
+        <input type="text" value={result} />
+        <div className="container_buttons">
+          <button name="" className="clear" onClick={ClearCalic}>
+            Clear
+          </button>
+          <button onClick={getValue} name="(">
+            (
+          </button>
+          <button onClick={getValue} name=")">
+            )
+          </button>
+          <button onClick={getValue} name="/">
+            /
+          </button>
+        </div>
+
+        <div className="container_buttons">
+          <button onClick={getValue} name="7">
+            7
+          </button>
+          <button onClick={getValue} name="8">
+            8
+          </button>
+          <button onClick={getValue} name="9">
+            9
+          </button>
+          <button onClick={getValue} name="*">
+            *
+          </button>
+        </div>
+
+        <div className="container_buttons">
+          <button onClick={getValue} name="4">
+            4
+          </button>
+          <button onClick={getValue} name="5">
+            5
+          </button>
+          <button onClick={getValue} name="6">
+            6
+          </button>
+          <button onClick={getValue} name="-">
+            -
+          </button>
+        </div>
+
+        <div className="container_buttons">
+          <button onClick={getValue} name="1">
+            1
+          </button>
+          <button onClick={getValue} name="2">
+            2
+          </button>
+          <button onClick={getValue} name="3">
+            3
+          </button>
+          <button onClick={getValue} name="+">
+            +
+          </button>
+        </div>
+
+        <div className="container_buttons">
+          <button onClick={deleteCalic} name="Del">
+            Del
+          </button>
+          <button onClick={getValue} name=".">
+            .
+          </button>
+          <button onClick={getValue} name="0">
+            0
+          </button>
+          <button onClick={CalicValue} name="=">
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
